e0efa6a6657961ff193c02e86a8d47dbce30ba79
matthijs <matthijs@heavymetalpenguin.com>
2018-06-23 15:18:40 +0200

    Finetune the Foursquare query

--- src/services/FoursquareService.js
+++ src/services/FoursquareService.js
@@ -7,6 +7,14 @@ export default {
   CLIENT_SECRET: 'VDQ0SJ3QEEHZDCHR1HOBFW5CGKMWYFUEAFVYM3OY3XKQSFNI',
   VERSION: '20180623',
 
+  CATEGORIES: {
+    TOURIST_INFO: '4f4530164b9074f6e4fb00ff',
+    CHURCHES: '4bf58dd8d48988d131941735',
+    OBSERVATORY: '5744ccdfe4b0c0459246b4d9',
+    RECREATION: '4d4b7105d754a06377d81259',
+    FOOD: '4d4b7105d754a06374d81259'
+  },
+
   searchPlacesNearLocation(latLong: object) {
 
     const url = this.parametrizeURL('venues/search', {
@@ -34,10 +42,15 @@ export default {
 
     }
 
+    url += `categoryId=${this.enumerateCategories()}&`;
     url += `client_id=${this.CLIENT_ID}&client_secret=${this.CLIENT_SECRET}&v=${this.VERSION}&limit=50`;
 
     return url;
 
+  },
+
+  enumerateCategories() {
+    return Object.values(this.CATEGORIES).join(',');
   }
 
 };
