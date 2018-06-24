e413d00a5b4d6fcfdd06ed5c24f94d6294cb4bbb
matthijs <matthijs@heavymetalpenguin.com>
2018-06-24 22:47:04 +0200

    a little cleanup

--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -6,38 +6,39 @@ import FoursquareService from '../../services/FoursquareService';
 
 const PlaceDetails = props => {
 
-  const photo = props.place.photos.groups[0].items[0];
+  const place = props.place,
+        photo = place.photos.groups[0].items[0];
 
   return (
     <div id="place-details" className="col-sm-12 col-md-8 col-lg-5 mt-md-3">
 
       <div className="place-image">
-        <img className="w-100" src={FoursquareService.getPhotoUrlForPhoto(photo)} alt={props.place.name} />
+        <img className="w-100" src={FoursquareService.getPhotoUrlForPhoto(photo)} alt={place.name} />
       </div>
 
       <div className="place-heading">
-        <h3>{props.place.name}</h3>
-        <h4>{props.place.categories[0].name}</h4>
+        <h3>{place.name}</h3>
+        <h4>{place.categories[0].name}</h4>
       </div>
 
-      {props.place.description &&
+      {place.description &&
       <div className="place-description mt-2">
-        {props.place.description}
+        {place.description}
       </div>}
 
-      {props.place.hours &&
-      <div className="place-hours mt-2">{props.place.hours.status}</div>}
+      {place.hours &&
+      <div className="place-hours mt-2">{place.hours.status}</div>}
 
-      {props.place.location.formattedAddress &&
+      {place.location.formattedAddress &&
       <div className="place-address mt-2">
-        <div>{props.place.location.formattedAddress[0]}</div>
-        <div>{props.place.location.formattedAddress[1]}</div>
-        <div>{props.place.location.formattedAddress[2]}</div>
+        <div>{place.location.formattedAddress[0]}</div>
+        <div>{place.location.formattedAddress[1]}</div>
+        <div>{place.location.formattedAddress[2]}</div>
       </div>
       }
 
-      {props.place.url && <div className="place-website mt-2">
-        <span>Website: </span><a href={props.place.url} target="_blank">{props.place.url}</a>
+      {place.url && <div className="place-website mt-2">
+        <span>Website: </span><a href={place.url} target="_blank">{place.url}</a>
       </div>}
     </div>
   );
