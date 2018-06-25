cc8a35909adcd381a84ebb62ceaf0e0a5776036b
Matthijs Rijken <matthijs.rijken@medicore.nl>
2018-06-25 13:13:24 +0200

    Enable tabbing and selecting through the list of places with the keyboard

--- src/components/Sidebar/PlaceItem/PlaceItem.js
+++ src/components/Sidebar/PlaceItem/PlaceItem.js
@@ -7,9 +7,18 @@ export default class PlaceItem extends Component {
     this.props.onClick(this.props.place);
   };
 
+  onKeyUp = e => {
+
+    // if Enter is pressed, treat it as a click
+    if (e.which === 13) {
+      this.onClick();
+    }
+
+  };
+
   render() {
     return (
-      <div className={`place-item p-2${this.props.place.isActive ? ' active': ''}`} onClick={this.onClick}>{this.props.place.name}</div>
+      <div tabIndex="0" className={`place-item p-2${this.props.place.isActive ? ' active': ''}`} onClick={this.onClick} onKeyUp={this.onKeyUp}>{this.props.place.name}</div>
     );
   }
 
